32be69ec0d4d5f4757b78789205894a82b37c7e3
jyoniki <72187229+jyoniki@users.noreply.github.com>
2020-10-28 23:58:43 +0900

    リクエストのやり直し

--- test/controllers/furimas_controller_test.rb
+++ test/controllers/furimas_controller_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class FurimasControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
     get furimas_index_url
-    assert_response :success
+    assert_response :success   
   end
 end
