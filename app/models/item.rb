161546067097738b9b220132ad987b85eba11709
jyoniki <72187229+jyoniki@users.noreply.github.com>
2020-10-29 14:33:28 +0900

    バリデーションの設定
    アクティブハッシュの導入もした

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
-  has_one: purchase
-  belongs_to: user
+  has_one :purchase
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :description_item
+    validates :category_id
+    validates :condition_id
+    validates :postage_payes_id
+    validates :prefecture_id
+    validates :handling_tims_id
+    validates :price
+    validates :user, foreign_key: true
+  end
 end
