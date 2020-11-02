8bf258d2954ae38a3c8f5f8b61787b9e263eb4c2
jyoniki <72187229+jyoniki@users.noreply.github.com>
2020-11-02 15:19:07 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -43,25 +43,25 @@ describe Item do
       end
 
       it 'category_idが1だと登録できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
 
       it 'postage_payes_idが1だと登録できない' do
-        @item.postage_payes_id = '1'
+        @item.postage_payes_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Postage payes must be other than 1')
       end
 
       it 'prefecture_idが1だと登録できない' do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
 
       it 'handling_tims_idが1だと登録できない' do
-        @item.handling_tims_id = '1'
+        @item.handling_tims_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Handling tims must be other than 1')
       end
@@ -73,13 +73,13 @@ describe Item do
       end
 
       it 'priceが299円だと登録できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
 
       it 'priceが10000000円だと登録できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
