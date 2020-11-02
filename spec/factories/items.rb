7b5e9679035622ea0351d32233fab8adc37aaddd
jyoniki <72187229+jyoniki@users.noreply.github.com>
2020-11-02 15:08:33 +0900

    修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name              { '物' }
     description_item  { 'そこらへんの物' }
-    category_id       { '3' }
-    condition_id      { '3' }
-    postage_payes_id  { '3' }
-    prefecture_id     { '3' }
-    handling_tims_id  { '3' }
-    price             { '600' }
+    category_id       { 3 }
+    condition_id      { 3 }
+    postage_payes_id  { 3 }
+    prefecture_id     { 3 }
+    handling_tims_id  { 3 }
+    price             { 600 }
     user
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
