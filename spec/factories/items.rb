b37a1218b90137d0dfbc7ea3fb0e07f6b93caa40
jyoniki <72187229+jyoniki@users.noreply.github.com>
2020-11-02 12:43:05 +0900

    テストコードの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,6 +8,7 @@ FactoryBot.define do
     prefecture_id     {'3'}
     handling_tims_id  {'3'}
     price             {'600'}
+    user
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,7 +7,7 @@ describe Item do
   describe '商品出品機能' do
     context '商品出品がうまくいくとき' do
       it "全ての条件がクリアできてれば出品できること" do
-      expect(@item).to be_valid
+        expect(@item).to be_valid
       end
     end
 
@@ -23,67 +23,67 @@ describe Item do
       it 'nameがないと登録できない' do
         @item.name = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Name must exist"）
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
 
       it 'nameが40字以上だと登録できない' do
         @item.name = 'a'*41
         @item.valid?
-        expect(@item.errors.full_messages).to include("Name can't be blank"）
+        expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
       end
 
       it 'description_itemがないと登録できない' do
         @item.description_item = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Description item can't be blank"）
+        expect(@item.errors.full_messages).to include("Description item can't be blank")
       end
 
       it 'description_itemが1000文字以上だと登録できない' do
         @item.description_item = 'a'*1001
         @item.valid?
-        expect(@item.errors.full_messages).to include("Description item can't be blank"）
+        expect(@item.errors.full_messages).to include("Description item is too long (maximum is 1000 characters)")
       end
 
       it 'category_idが1だと登録できない' do
         @item.category_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category id can't be blank"）
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
 
       it 'postage_payes_idが1だと登録できない' do
         @item.postage_payes_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Postage payes id can't be blank"）
+        expect(@item.errors.full_messages).to include("Postage payes must be other than 1")
       end
 
       it 'prefecture_idが1だと登録できない' do
         @item.prefecture_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture id can't be blank"）
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
 
       it 'handling_tims_idが1だと登録できない' do
         @item.handling_tims_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Handling tims id can't be blank"）
+        expect(@item.errors.full_messages).to include("Handling tims must be other than 1")
       end
 
       it 'priceがないと登録できない' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank"）
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
       it 'priceが299円だと登録できない' do
         @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank"）
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
 
       it 'priceが10000000円だと登録できない' do
         @item.price = '10000000'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank"）
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
     end
   end
