f8c9ebda82e1e9f466b926b91e3543d3ee8b498a
jyoniki <72187229+jyoniki@users.noreply.github.com>
2020-11-02 12:21:52 +0900

    商品出品のテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,90 @@
 require 'rails_helper'
+describe Item do
+  before do
+    @item = FactoryBot.build(:item)
+  end
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品機能' do
+    context '商品出品がうまくいくとき' do
+      it "全ての条件がクリアできてれば出品できること" do
+      expect(@item).to be_valid
+      end
+    end
+
+
+    context '新規登録がうまくいかないとき' do
+
+      it 'imageがないと登録できない' do
+       @item.image = nil
+       @item.valid?
+       expect(@item.errors.full_messages).to include("Image can't be blank")
+     end
+
+      it 'nameがないと登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name must exist"）
+      end
+
+      it 'nameが40字以上だと登録できない' do
+        @item.name = 'a'*41
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank"）
+      end
+
+      it 'description_itemがないと登録できない' do
+        @item.description_item = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description item can't be blank"）
+      end
+
+      it 'description_itemが1000文字以上だと登録できない' do
+        @item.description_item = 'a'*1001
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description item can't be blank"）
+      end
+
+      it 'category_idが1だと登録できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category id can't be blank"）
+      end
+
+      it 'postage_payes_idが1だと登録できない' do
+        @item.postage_payes_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage payes id can't be blank"）
+      end
+
+      it 'prefecture_idが1だと登録できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture id can't be blank"）
+      end
+
+      it 'handling_tims_idが1だと登録できない' do
+        @item.handling_tims_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Handling tims id can't be blank"）
+      end
+
+      it 'priceがないと登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank"）
+      end
+
+      it 'priceが299円だと登録できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank"）
+      end
+
+      it 'priceが10000000円だと登録できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank"）
+      end
+    end
+  end
 end
