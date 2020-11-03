89853ccc3300ca1c51a909714a47e15477885ca3
jyoniki <72187229+jyoniki@users.noreply.github.com>
2020-11-03 17:37:33 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -18,10 +18,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-    
+  def show
+    @item = Item.find(params[:id])
   end
-  
 
   private
 
