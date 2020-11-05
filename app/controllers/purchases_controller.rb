bb00c5dfc550a987c7f0c4b3db3d16c380198b6c
jyoniki <72187229+jyoniki@users.noreply.github.com>
2020-11-05 17:56:35 +0900

    購入ページへの遷移設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,10 +2,11 @@ class PurchasesController < ApplicationController
 
   def index
     purchaseaddress = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
   end
   
   def create
-    
   end
   
+
 end
